9be6b35da620353d68bd190f594e6ddc5bac6333
ipriyam26 <79325116+ipriyam26@users.noreply.github.com>
2021-11-18 16:18:28 +0530

    event updated

--- lib/testfire.dart
+++ lib/testfire.dart
@@ -1,5 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:deep_pocket_1/getcourse.dart';
+import 'package:deep_pocket_1/models/course_read.dart';
+import 'package:deep_pocket_1/read_data.dart';
+import 'package:deep_pocket_1/screens/event/get_event.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 
 class test_fire extends StatefulWidget {
   const test_fire({Key? key}) : super(key: key);
@@ -8,45 +13,26 @@ class test_fire extends StatefulWidget {
   State<test_fire> createState() => _test_fireState();
 }
 
-class _test_fireState extends State<test_fire> {
-  Future<void> tryUpload() async {
-    final value = await FirebaseFirestore.instance
-        .collection("Courses")
-        .doc("Design")
-        .get();
+Future<void> printdata() async {
+  List<Course> data = await sendData().ReadJsonData();
+  for (int i = 0; i < data.length; i++) {
+    print(data[i].name);
   }
+}
 
+class _test_fireState extends State<test_fire> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("YOLO"),
       ),
-      body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection("Courses")
-              .doc("Details")
-              .collection("SubCatagory")
-              .doc()
-              .snapshots(),
-          builder: (context,
-              AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
-            if (!snapshot.hasData) {
-              return Text("data");
-            }
-            // ignore: avoid_print
-            print(snapshot.data.toString());
-            return Container();
-            // return Center(
-            //   child: ListView.builder(
-            //     itemCount: snapshot.data!.data()!.length,
-            //     itemBuilder: (context, index) {
-            //       print(snapshot.data!.data()![index].id);
-            //       return Text(snapshot.data!.data()![index].id);
-            //     },
-            //   ),
-            // );
-          }),
+      body: Container(
+        child: ElevatedButton(
+          child: Text("efef"),
+          onPressed: printdata,
+        ),
+      ),
     );
   }
 }
